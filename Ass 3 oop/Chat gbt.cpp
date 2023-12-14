0c03c61e3819b52585f9ca7b77f34f84a5b88ec4
esllam21 <148411825+esllam21@users.noreply.github.com>
2023-12-14 05:29:02 +0200

    Task 1,2

--- Ass 3 oop/Chat gbt.cpp
+++ Ass 3 oop/Chat gbt.cpp
@@ -1,150 +1,150 @@
-#include <iostream>
-#include <string>
-
-class Course {
-protected:
-    std::string name;
-    std::string code;
-    int minStudentLevel;
-    int creditHours;
-
-public:
-    Course() : name(""), code(""), minStudentLevel(0), creditHours(0) {}
-
-    Course(std::string name, int level, int creditHours)
-        : name(name), minStudentLevel(level), creditHours(creditHours) {
-        // Generate code based on class type
-        code = name + "_N" + std::to_string(level);
-    }
-
-    int getCreditHours() const {
-        return creditHours;
-    }
-
-    friend std::ostream& operator<<(std::ostream& os, const Course& course) {
-        os << "Code: " << course.code << ", Name: " << course.name
-            << ", Credit Hours: " << course.creditHours
-            << ", Min Student Level: " << course.minStudentLevel;
-        return os;
-    }
-};
-
-class Student {
-protected:
-    int level;
-    double grades[6];
-    Course* courseList[6];
-    int noOfCourses;
-
-public:
-    Student() : level(0), noOfCourses(0) {
-        for (int i = 0; i < 6; ++i) {
-            grades[i] = 0.0;
-            courseList[i] = nullptr;
-        }
-    }
-
-    virtual bool RegisterCourse(Course* course) {
-        if (noOfCourses >= 6 || level < course->minStudentLevel) {
-            return false;
-        }
-
-        courseList[noOfCourses++] = course;
-        return true;
-    }
-
-    void printRegisteredCourses() const {
-        for (int i = 0; i < noOfCourses; ++i) {
-            std::cout << *courseList[i] << ", Grade: " << grades[i] << std::endl;
-        }
-    }
-
-    virtual float CalculateExpenses() const = 0;
-
-    double getTotalGPA() const {
-        double totalPoints = 0.0;
-        double totalCredits = 0.0;
-
-        for (int i = 0; i < noOfCourses; ++i) {
-            totalPoints += getCourseGPA(grades[i]) * courseList[i]->getCreditHours();
-            totalCredits += static_cast<float>(courseList[i]->getCreditHours());
-        }
-
-        if (totalCredits == 0.0) {
-            return 0.0;
-        }
-
-        return totalPoints / totalCredits;
-    }
-
-    double getCourseGPA(double grade) const {
-        if (grade >= 90) {
-            return 4.0;
-        }
-        else if (grade >= 80) {
-            return 3.0;
-        }
-        else if (grade >= 70) {
-            return 2.0;
-        }
-        else if (grade >= 60) {
-            return 1.0;
-        }
-        else {
-            return 0.0;
-        }
-    }
-};
-
-class GeneralProgramStudent : public Student {
-public:
-    bool RegisterCourse(Course* course) override {
-        if (noOfCourses >= 6 || level < course->minStudentLevel) {
-            return false;
-        }
-
-        courseList[noOfCourses++] = course;
-        return true;
-    }
-
-    float CalculateExpenses() const override {
-        return static_cast<float>(level * 1000);
-    }
-};
-
-class SpecialProgramStudent : public Student {
-public:
-    bool RegisterCourse(Course* course) override {
-        if (noOfCourses >= 6) {
-            return false;
-        }
-
-        courseList[noOfCourses++] = course;
-        return true;
-    }
-
-    float CalculateExpenses() const override {
-        float totalCredits = 0.0;
-        for (int i = 0; i < noOfCourses; ++i) {
-            totalCredits += static_cast<float>(courseList[i]->getCreditHours());
-        }
-
-        return totalCredits * 500;
-    }
-};
-
-int main() {
-    // Example usage
-    GeneralProgramStudent generalStudent;
-    generalStudent.RegisterCourse(new Course("Math", 1, 3));
-    generalStudent.RegisterCourse(new Course("Physics", 2, 4));
-
-    SpecialProgramStudent specialStudent;
-    specialStudent.RegisterCourse(new Course("Computer Science", 1, 3));
-    specialStudent.RegisterCourse(new Course("Chemistry", 3, 4));
-
-    std::cout << "General Student Expenses: " << generalStudent.CalculateExpenses() << " EGP" << std::endl;
-    std::cout << "Special Student Expenses: " << specialStudent.CalculateExpenses() << " EGP" << std::endl;
-
-    return 0;
-}
+//#include <iostream>
+//#include <string>
+//
+//class Course {
+//protected:
+//    std::string name;
+//    std::string code;
+//    int minStudentLevel;
+//    int creditHours;
+//
+//public:
+//    Course() : name(""), code(""), minStudentLevel(0), creditHours(0) {}
+//
+//    Course(std::string name, int level, int creditHours)
+//        : name(name), minStudentLevel(level), creditHours(creditHours) {
+//        // Generate code based on class type
+//        code = name + "_N" + std::to_string(level);
+//    }
+//
+//    int getCreditHours() const {
+//        return creditHours;
+//    }
+//
+//    friend std::ostream& operator<<(std::ostream& os, const Course& course) {
+//        os << "Code: " << course.code << ", Name: " << course.name
+//            << ", Credit Hours: " << course.creditHours
+//            << ", Min Student Level: " << course.minStudentLevel;
+//        return os;
+//    }
+//};
+//
+//class Student {
+//protected:
+//    int level;
+//    double grades[6];
+//    Course* courseList[6];
+//    int noOfCourses;
+//
+//public:
+//    Student() : level(0), noOfCourses(0) {
+//        for (int i = 0; i < 6; ++i) {
+//            grades[i] = 0.0;
+//            courseList[i] = nullptr;
+//        }
+//    }
+//
+//    virtual bool RegisterCourse(Course* course) {
+//        if (noOfCourses >= 6 || level < course->minStudentLevel) {
+//            return false;
+//        }
+//
+//        courseList[noOfCourses++] = course;
+//        return true;
+//    }
+//
+//    void printRegisteredCourses() const {
+//        for (int i = 0; i < noOfCourses; ++i) {
+//            std::cout << *courseList[i] << ", Grade: " << grades[i] << std::endl;
+//        }
+//    }
+//
+//    virtual float CalculateExpenses() const = 0;
+//
+//    double getTotalGPA() const {
+//        double totalPoints = 0.0;
+//        double totalCredits = 0.0;
+//
+//        for (int i = 0; i < noOfCourses; ++i) {
+//            totalPoints += getCourseGPA(grades[i]) * courseList[i]->getCreditHours();
+//            totalCredits += static_cast<float>(courseList[i]->getCreditHours());
+//        }
+//
+//        if (totalCredits == 0.0) {
+//            return 0.0;
+//        }
+//
+//        return totalPoints / totalCredits;
+//    }
+//
+//    double getCourseGPA(double grade) const {
+//        if (grade >= 90) {
+//            return 4.0;
+//        }
+//        else if (grade >= 80) {
+//            return 3.0;
+//        }
+//        else if (grade >= 70) {
+//            return 2.0;
+//        }
+//        else if (grade >= 60) {
+//            return 1.0;
+//        }
+//        else {
+//            return 0.0;
+//        }
+//    }
+//};
+//
+//class GeneralProgramStudent : public Student {
+//public:
+//    bool RegisterCourse(Course* course) override {
+//        if (noOfCourses >= 6 || level < course->minStudentLevel) {
+//            return false;
+//        }
+//
+//        courseList[noOfCourses++] = course;
+//        return true;
+//    }
+//
+//    float CalculateExpenses() const override {
+//        return static_cast<float>(level * 1000);
+//    }
+//};
+//
+//class SpecialProgramStudent : public Student {
+//public:
+//    bool RegisterCourse(Course* course) override {
+//        if (noOfCourses >= 6) {
+//            return false;
+//        }
+//
+//        courseList[noOfCourses++] = course;
+//        return true;
+//    }
+//
+//    float CalculateExpenses() const override {
+//        float totalCredits = 0.0;
+//        for (int i = 0; i < noOfCourses; ++i) {
+//            totalCredits += static_cast<float>(courseList[i]->getCreditHours());
+//        }
+//
+//        return totalCredits * 500;
+//    }
+//};
+//
+//int main() {
+//    // Example usage
+//    GeneralProgramStudent generalStudent;
+//    generalStudent.RegisterCourse(new Course("Math", 1, 3));
+//    generalStudent.RegisterCourse(new Course("Physics", 2, 4));
+//
+//    SpecialProgramStudent specialStudent;
+//    specialStudent.RegisterCourse(new Course("Computer Science", 1, 3));
+//    specialStudent.RegisterCourse(new Course("Chemistry", 3, 4));
+//
+//    std::cout << "General Student Expenses: " << generalStudent.CalculateExpenses() << " EGP" << std::endl;
+//    std::cout << "Special Student Expenses: " << specialStudent.CalculateExpenses() << " EGP" << std::endl;
+//
+//    return 0;
+//}
--- Ass 3 oop/Try 2.cpp
+++ Ass 3 oop/Try 2.cpp
@@ -12,6 +12,7 @@ public:
 	Course(string Name, int level, int Hours) :name(Name), Min_Student_level(level), CreditHours(Hours) {};
 	friend ostream& operator<<(ostream& out, const Course& obj);
 	int getHours() { return CreditHours; }
+	int getLevel() { return Min_Student_level; }
 };
 class NormalCourse :public Course {
 	NormalCourse() :Course() {};
@@ -26,6 +27,7 @@ class SummerCourse :public Course {
 	}
 };
 class Student {
+protected:
 	int level;
 	float grades[6];
 	Course* CourseList[6];
@@ -50,37 +52,105 @@ public:
 		else return false;
 	}
 	double getTotalGPA() {
-		double totalGPA = 0;
+		double TotalGPA = 0;
 		double TotalHours = 0;
 		for (int i = 0; i < noOfCourses; i++) {
 			TotalHours += CourseList[i]->getHours();
-
+			TotalGPA += getCourseGPA(grades[i]) * CourseList[i]->getHours();
 		}
+		if (TotalHours == 0.0)
+			return 0.0;
+		return TotalGPA / TotalHours;
+	}
+	double getCourseGPA(double grade) {
+		string GPA_Letter;
+		if (grade >= 98 && grade <= 100) {
+			GPA_Letter = "A+";
+			return 4.0;
+		}
+		else if (grade >= 93 && grade <= 97) {
+			GPA_Letter = "A";
+			return 4.0;
+		}
+		else if (grade >= 90 && grade <= 92) {
+			GPA_Letter = "A-";
+			return 3.7;
+		}
+		else if (grade >= 87 && grade <= 89) {
+			GPA_Letter = "B+";
+			return 3.3;
+		}
+		else if (grade >= 83 && grade <= 86) {
+			GPA_Letter = "B";
+			return 3.0;
+		}
+		else if (grade >= 80 && grade <= 82) {
+			GPA_Letter = "B-";
+			return 2.7;
+		}
+		else if (grade >= 77 && grade <= 79) {
+			GPA_Letter = "C+";
+			return 2.3;
+		}
+		else if (grade >= 73 && grade <= 76) {
+			GPA_Letter = "C";
+			return 2.0;
+		}
+		else if (grade >= 70 && grade <= 72) {
+			GPA_Letter = "C-";
+			return 1.7;
+		}
+		else if (grade >= 67 && grade <= 69) {
+			GPA_Letter = "D+";
+			return 1.3;
+		}
+		else if (grade >= 63 && grade <= 66) {
+			GPA_Letter = "D";
+			return 1.0;
+		}
+		else if (grade >= 60 && grade <= 62) {
+			GPA_Letter = "D-";
+			return 0.7;
+		}
+		else if (grade >= 0 && grade <= 59) {
+			GPA_Letter = "F";
+			return 0.0;
+		}
+		else cout << "Please enter valid Grade\n";
 	}
 };
+class GenralProgramStudent :public Student {
+	bool RegisterCourse(Course* course) {
+		if (noOfCourses >= 0 && noOfCourses < 6 && course->getLevel() <= level) {
+			CourseList[noOfCourses] = course;
+			return true;
+		}
+		else return false;
+	}
+	float CalculateExpenses() {
+		float Expenses = level*1000;
+		return Expenses;
+	}
+};
+class SpecialProgramStudent :public Student {
+	bool RegisterCourse(Course*course) {
+		if (noOfCourses >= 0 && noOfCourses < 6) {
+			CourseList[noOfCourses] = course;
+			return true;
+		}
+		else return false;
+	}
+	float CalculateExpenses() {
+		float Expenses;
+		float TotalHours = 0;
+		for (int i = 0; i < noOfCourses; i++)
+			TotalHours += CourseList[i]->getHours();
+		Expenses = TotalHours * 500;
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	}
+};
 
 ostream& operator<<(ostream& out, const Course& obj) {
 	out << "Course Information: \nCode: " << obj.code << endl;
